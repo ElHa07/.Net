5f8bb1fa6bbb45a04de57522bb9b00daff43dcc9
Jefferson Garcia <jemefox007@gmail.com>
2021-02-09 20:57:23 -0300

    Update Serie.cs

--- Dio.Series/Classes/Serie.cs
+++ Dio.Series/Classes/Serie.cs
@@ -1,9 +1,17 @@
 namespace Dio.Series
 {
-    
+
     public class Serie : EntidadeBase
     {
+        // Atributos
 
+        private Genero { get; set; }
+
+        private string Titulo { get; set; }
+
+        private string Descricao { get; set; }
+
+        private int Ano { get; set; }
     }
 
 }
